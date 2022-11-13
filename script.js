c0a2c211fbc832dcab1fc8b39785cab6547762f8
Param Thakkar <pht0104@gmail.com>
2022-11-12 21:50:21 -0500

    Comment js code

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-window.onload = function () {
-  var xhttp = new XMLHttpRequest();
+// window.onload = function () {
+//   var xhttp = new XMLHttpRequest();
 
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      data = JSON.parse(this.responseText);
-      document.querySelector("#links").innerHTML =
-        data.total_links + " unique links";
-      document.querySelector("#clicks").innerHTML =
-        data.total_visits + " total clicks";
-      document.querySelector("h1").innerHTML = "Stats";
-    } else if (this.readyState == 4 && this.status == 404) {
-    }
-  };
+//   xhttp.onreadystatechange = function () {
+//     if (this.readyState == 4 && this.status == 200) {
+//       data = JSON.parse(this.responseText);
+//       document.querySelector("#links").innerHTML =
+//         data.total_links + " unique links";
+//       document.querySelector("#clicks").innerHTML =
+//         data.total_visits + " total clicks";
+//       document.querySelector("h1").innerHTML = "Stats";
+//     } else if (this.readyState == 4 && this.status == 404) {
+//     }
+//   };
 
-  xhttp.open("GET", "https://api.1pt.co/getStats", true);
-  xhttp.send();
-};
+//   xhttp.open("GET", "https://api.1pt.co/getStats", true);
+//   xhttp.send();
+// };
